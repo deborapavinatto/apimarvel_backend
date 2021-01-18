c04357941951a3bf7488080dab7a58c0395f028a
teeusdm <teeusdm@gmail.com>
2021-01-17 23:13:28 -0300

    first commit

--- src/router.js
+++ src/router.js
@@ -1,25 +1,10 @@
 import { Router } from 'express';
 import UseController from './app/controllers/UseController';
-// import Services from './services';
 
 const routes = new Router();
-// const services = new Services();
-
-/*
-routes.get('/', (req, res) => {
-  res.json(services.getUrl());
-}); */
-
-/*  async function teste() {
-  await services.getUrl();
-
-} */
 
 routes.get('/characters', UseController.index);
 
 routes.get('/characters/:characterid', UseController.show);
-
-// routes.get('/characters/pages/:page', UseController.offset);
-// teste();
 
 export default routes;
